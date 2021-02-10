af17d03c2c3fc66f588f12b76258ef390f6ee19c
Robert Szuba <robert@onegini.com>
2021-02-10 17:02:38 +0100

    CORDOPLUG-441 Use Cordova Plugin Push for FCM

--- src/views/App.vue
+++ src/views/App.vue
@@ -30,7 +30,28 @@
     },
 
     methods: {
-      onDeviceReady: function () {
+      onDeviceReady: function() {
+        const pusher = PushNotification.init({android: {},ios: {alert: "true", badge: "true", sound: "true"}});
+        pusher.on('registration', (data) => {
+          window.localStorage.setItem("fcmToken", data.registrationId);
+        });
+
+        pusher.on('notification', (data) => {
+          if (onegini.mobileAuth.push.canHandlePushMessage(data.additionalData)) {
+            onegini.mobileAuth.push.handlePushMessage(data.additionalData)
+              .catch((err) => navigator.notification.alert('Push message error: ' + err.description));
+          } else {
+            var message = (data.title ? data.title + " " : "") + (data.message ? data.message : "");
+            if (message.length > 0) {
+              navigator.notification.alert(message);
+            }
+          }
+        });
+
+        pusher.on('error', (e) => {
+          navigator.notification.alert('Push message error: ' + e.message);
+        });
+
         this.startOnegini();
       },
 
